877700f47fa3fe20d6be65816be28c2e9afebcd4
Antanidoss <antosci2002@gmail.com>
2021-01-04 21:28:07 +0300

    refactoring

--- src/FinanceMeneger.Api/Program.cs
+++ src/FinanceMeneger.Api/Program.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using FinanceManager.Api.Helpers;
 using FinanceManager.Services.Common.Interfaces;
-using FinanceManager.Services.Implementation;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FinanceManeger.Api
 {
